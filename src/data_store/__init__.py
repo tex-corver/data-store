9bbf7fea581d2eeb5f98b49c1200cf6bbd110a07
delus <kid.clone.3@gmail.com>
2025-07-29 00:58:28 +0700

    refactor: move code into object_store

--- src/data_store/__init__.py
+++ src/data_store/__init__.py
@@ -1,3 +1,4 @@
-from .configurations import ObjectStoreConfiguration, ObjectStoreConnectionConfiguration
-from .models import Bucket, ObjectMetadata, Object
-from .store import *
+from .nosql_store import *
+from .object_store import *
+from .sql_store import *
+from .vector_store import *
--- tests/e2e/test_client.py
+++ tests/e2e/test_client.py
@@ -1,6 +1,17 @@
+import polars as pl
+
 import data_store
 
 
-class TestDataStore:
+class TestObjectStore:
     def test_init(self):
         store = data_store.ObjectStore()
+
+
+class TestNoSQLStore:
+    def test_init(self):
+        store = data_store.NoSQLStore()
+        assert store.config is not None
+        data = store.load_data()
+        assert data is not None
+        assert isinstance(data, pl.DataFrame)
